710d0b50c2746addc0cf87adb63ac855d6c34783
Nicholas Ritchie <nicholas.ritchie@nist.gov>
2025-09-17 13:18:21 -0400

    Adding batch files for processing GM IIIB data

---
+++ N132962/Quantification/Glass Mount IIIB/20 keV/compare.bat
@@ -0,0 +1,54 @@
+spex compare "N132962" "'NIST K1001'" "GM IIIB K1001[0][4].msa" "GM IIIB K1001[1][4].msa" "GM IIIB K1001[2][4].msa" "GM IIIB K1001[3][4].msa" "GM IIIB K1001[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1008'" "GM IIIB K1008[0][4].msa" "GM IIIB K1008[1][4].msa" "GM IIIB K1008[2][4].msa" "GM IIIB K1008[3][4].msa" "GM IIIB K1008[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1010'" "GM IIIB K1010[0][4].msa" "GM IIIB K1010[1][4].msa" "GM IIIB K1010[2][4].msa" "GM IIIB K1010[3][4].msa" "GM IIIB K1010[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1012'" "GM IIIB K1012[0][4].msa" "GM IIIB K1012[1][4].msa" "GM IIIB K1012[2][4].msa" "GM IIIB K1012[3][4].msa" "GM IIIB K1012[4][4].msa"  "GM IIIB K1012[5][4].msa" "GM IIIB K1012[6][4].msa" "GM IIIB K1012[7][4].msa" "GM IIIB K1012[8][4].msa" "GM IIIB K1012[9][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1053'" "GM IIIB K1053[0][4].msa" "GM IIIB K1053[1][4].msa" "GM IIIB K1053[2][4].msa" "GM IIIB K1053[3][4].msa" "GM IIIB K1053[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1070'" "GM IIIB K1070[0][4].msa" "GM IIIB K1070[1][4].msa" "GM IIIB K1070[2][4].msa" "GM IIIB K1070[3][4].msa" "GM IIIB K1070[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1080'" "GM IIIB K1080[0][4].msa" "GM IIIB K1080[1][4].msa" "GM IIIB K1080[2][4].msa" "GM IIIB K1080[3][4].msa" "GM IIIB K1080[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1092'" "GM IIIB K1092[0][4].msa" "GM IIIB K1092[1][4].msa" "GM IIIB K1092[2][4].msa" "GM IIIB K1092[3][4].msa" "GM IIIB K1092[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1098'" "GM IIIB K1098[0][4].msa" "GM IIIB K1098[1][4].msa" "GM IIIB K1098[2][4].msa" "GM IIIB K1098[3][4].msa" "GM IIIB K1098[4][4].msa" 
+spex compare "N132962" "'NIST K1132'" "GM IIIB K1132[0][4].msa" "GM IIIB K1132[1][4].msa" "GM IIIB K1132[2][4].msa" "GM IIIB K1132[3][4].msa" "GM IIIB K1132[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1229'" "GM IIIB K1229[0][4].msa" "GM IIIB K1229[1][4].msa" "GM IIIB K1229[2][4].msa" "GM IIIB K1229[3][4].msa" "GM IIIB K1229[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1234'" "GM IIIB K1234[0][4].msa" "GM IIIB K1234[1][4].msa" "GM IIIB K1234[2][4].msa" "GM IIIB K1234[3][4].msa" "GM IIIB K1234[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K1235'" "GM IIIB K1235[0][4].msa" "GM IIIB K1235[1][4].msa" "GM IIIB K1235[2][4].msa" "GM IIIB K1235[3][4].msa" "GM IIIB K1235[4][4].msa" 
+spex compare "N132962" "'NIST K1236'" "GM IIIB K1236[0][4].msa" "GM IIIB K1236[1][4].msa" "GM IIIB K1236[2][4].msa" "GM IIIB K1236[3][4].msa" "GM IIIB K1236[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K227'" "GM IIIB K227[0][4].msa" "GM IIIB K227[1][4].msa" "GM IIIB K227[2][4].msa" "GM IIIB K227[3][4].msa" "GM IIIB K227[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K229'" "GM IIIB K229[0][4].msa" "GM IIIB K229[1][4].msa" "GM IIIB K229[2][4].msa" "GM IIIB K229[3][4].msa" "GM IIIB K229[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K230'" "GM IIIB K230[0][4].msa" "GM IIIB K230[1][4].msa" "GM IIIB K230[2][4].msa" "GM IIIB K230[3][4].msa" "GM IIIB K230[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K240'" "GM IIIB K240[0][4].msa" "GM IIIB K240[1][4].msa" "GM IIIB K240[2][4].msa" "GM IIIB K240[3][4].msa" "GM IIIB K240[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K249'" "GM IIIB K249[0][4].msa" "GM IIIB K249[1][4].msa" "GM IIIB K249[2][4].msa" "GM IIIB K249[3][4].msa" "GM IIIB K249[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K251'" "GM IIIB K251[0][4].msa" "GM IIIB K251[1][4].msa" "GM IIIB K251[2][4].msa" "GM IIIB K251[3][4].msa" "GM IIIB K251[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K252'" "GM IIIB K252[0][4].msa" "GM IIIB K252[1][4].msa" "GM IIIB K252[2][4].msa" "GM IIIB K252[3][4].msa" "GM IIIB K252[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K253'" "GM IIIB K253[0][4].msa" "GM IIIB K253[1][4].msa" "GM IIIB K253[2][4].msa" "GM IIIB K253[3][4].msa" "GM IIIB K253[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K309'" "GM IIIB K309[0][4].msa" "GM IIIB K309[1][4].msa" "GM IIIB K309[2][4].msa" "GM IIIB K309[3][4].msa" "GM IIIB K309[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K326'" "GM IIIB K326[0][4].msa" "GM IIIB K326[1][4].msa" "GM IIIB K326[2][4].msa" "GM IIIB K326[3][4].msa" "GM IIIB K326[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K366'" "GM IIIB K366[0][4].msa" "GM IIIB K366[1][4].msa" "GM IIIB K366[2][4].msa" "GM IIIB K366[3][4].msa" "GM IIIB K366[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K309'" "GM IIIB K369[0][4].msa" "GM IIIB K369[1][4].msa" "GM IIIB K369[2][4].msa" "GM IIIB K369[3][4].msa" "GM IIIB K369[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K371'" "GM IIIB K371[0][4].msa" "GM IIIB K371[1][4].msa" "GM IIIB K371[2][4].msa" "GM IIIB K371[3][4].msa" "GM IIIB K371[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K373'" "GM IIIB K373[0][4].msa" "GM IIIB K373[1][4].msa" "GM IIIB K373[2][4].msa" "GM IIIB K373[3][4].msa" "GM IIIB K373[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K376'" "GM IIIB K376[0][4].msa" "GM IIIB K376[1][4].msa" "GM IIIB K376[2][4].msa" "GM IIIB K376[3][4].msa" "GM IIIB K376[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K378'" "GM IIIB K378[0][4].msa" "GM IIIB K378[1][4].msa" "GM IIIB K378[2][4].msa" "GM IIIB K378[3][4].msa" "GM IIIB K378[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K408'" "GM IIIB K408[0][4].msa" "GM IIIB K408[1][4].msa" "GM IIIB K408[2][4].msa" "GM IIIB K408[3][4].msa" "GM IIIB K408[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K409'" "GM IIIB K409[0][4].msa" "GM IIIB K409[1][4].msa" "GM IIIB K409[2][4].msa" "GM IIIB K409[3][4].msa" "GM IIIB K409[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K411'" "GM IIIB K411[0][4].msa" "GM IIIB K411[1][4].msa" "GM IIIB K411[2][4].msa" "GM IIIB K411[3][4].msa" "GM IIIB K411[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K412'" "GM IIIB K412[0][4].msa" "GM IIIB K412[1][4].msa" "GM IIIB K412[2][4].msa" "GM IIIB K412[3][4].msa" "GM IIIB K412[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K453'" "GM IIIB K453[0][4].msa" "GM IIIB K453[1][4].msa" "GM IIIB K453[2][4].msa" "GM IIIB K453[3][4].msa" "GM IIIB K453[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K456'" "GM IIIB K456[0][4].msa" "GM IIIB K456[1][4].msa" "GM IIIB K456[2][4].msa" "GM IIIB K456[3][4].msa" "GM IIIB K456[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K458'" "GM IIIB K458[0][4].msa" "GM IIIB K458[1][4].msa" "GM IIIB K458[2][4].msa" "GM IIIB K458[3][4].msa" "GM IIIB K458[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K489'" "GM IIIB K489[0][4].msa" "GM IIIB K489[1][4].msa" "GM IIIB K489[2][4].msa" "GM IIIB K489[3][4].msa" "GM IIIB K489[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K491'" "GM IIIB K491[0][4].msa" "GM IIIB K491[1][4].msa" "GM IIIB K491[2][4].msa" "GM IIIB K491[3][4].msa" "GM IIIB K491[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K493'" "GM IIIB K493[0][4].msa" "GM IIIB K493[1][4].msa" "GM IIIB K493[2][4].msa" "GM IIIB K493[3][4].msa" "GM IIIB K493[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K495'" "GM IIIB K495[0][4].msa" "GM IIIB K495[1][4].msa" "GM IIIB K495[2][4].msa" "GM IIIB K495[3][4].msa" "GM IIIB K495[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K496'" "GM IIIB K496[0][4].msa" "GM IIIB K496[1][4].msa" "GM IIIB K496[2][4].msa" "GM IIIB K496[3][4].msa" "GM IIIB K496[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K497'" "GM IIIB K497[0][4].msa" "GM IIIB K497[1][4].msa" "GM IIIB K497[2][4].msa" "GM IIIB K497[3][4].msa" "GM IIIB K497[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K508'" "GM IIIB K508[0][4].msa" "GM IIIB K508[1][4].msa" "GM IIIB K508[2][4].msa" "GM IIIB K508[3][4].msa" "GM IIIB K508[4][4].msa" "GM IIIB K508[5][4].msa" "GM IIIB K508[6][4].msa" --sum 25.0
+spex compare "N132962" "'NIST K521'" "GM IIIB K521[0][4].msa" "GM IIIB K521[1][4].msa" "GM IIIB K521[2][4].msa" "GM IIIB K521[3][4].msa" "GM IIIB K521[4][4].msa" "GM IIIB K521[5][4].msa" "GM IIIB K521[6][4].msa" --sum 25.0
+spex compare "N132962" "'NIST K523'" "GM IIIB K523[0][4].msa" "GM IIIB K523[1][4].msa" "GM IIIB K523[2][4].msa" "GM IIIB K523[3][4].msa" "GM IIIB K523[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K873'" "GM IIIB K873[0][4].msa" "GM IIIB K873[1][4].msa" "GM IIIB K873[2][4].msa" "GM IIIB K873[3][4].msa" "GM IIIB K873[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K919'" "GM IIIB K919[0][4].msa" "GM IIIB K919[1][4].msa" "GM IIIB K919[2][4].msa" "GM IIIB K919[3][4].msa" "GM IIIB K919[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K961'" "GM IIIB K961[0][4].msa" "GM IIIB K961[1][4].msa" "GM IIIB K961[2][4].msa" "GM IIIB K961[3][4].msa" "GM IIIB K961[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K963'" "GM IIIB K963[0][4].msa" "GM IIIB K963[1][4].msa" "GM IIIB K963[2][4].msa" "GM IIIB K963[3][4].msa" "GM IIIB K963[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K968'" "GM IIIB K968[0][4].msa" "GM IIIB K968[1][4].msa" "GM IIIB K968[2][4].msa" "GM IIIB K968[3][4].msa" "GM IIIB K968[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K975'" "GM IIIB K975[0][4].msa" "GM IIIB K975[1][4].msa" "GM IIIB K975[2][4].msa" "GM IIIB K975[3][4].msa" "GM IIIB K975[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K978'" "GM IIIB K978[0][4].msa" "GM IIIB K978[1][4].msa" "GM IIIB K978[2][4].msa" "GM IIIB K978[3][4].msa" "GM IIIB K978[4][4].msa" --sum 3.0
+spex compare "N132962" "'NIST K979'" "GM IIIB K979[0][4].msa" "GM IIIB K979[1][4].msa" "GM IIIB K979[2][4].msa" "GM IIIB K979[3][4].msa" "GM IIIB K979[4][4].msa" --sum 3.0
